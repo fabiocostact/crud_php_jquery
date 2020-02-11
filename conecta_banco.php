c7e6d058e105ba1c919fa1a81e7f34c9e2413cd4
Fábio Costa <55239420+fabiocostact@users.noreply.github.com>
2020-02-11 17:44:26 -0300

    Update conecta_banco.php

--- conecta_banco.php
+++ conecta_banco.php
@@ -5,13 +5,6 @@ $dsn = 'mysql:dbname=fideliza;host=localhost';
 $user = 'root';
 $password = '';
 
-//http://fizeliza.gmpti.com/
-
-/*
-$dsn = 'mysql:dbname=gmptic21_fideliza;host=cpanel.gmpti.com';
-$user = 'gmptic21_root';
-$password = 'a2dd80_apps';*/
-
 try {
  	$con = new PDO($dsn, $user, $password);
 	$con->exec("SET CHARACTER SET utf8");
